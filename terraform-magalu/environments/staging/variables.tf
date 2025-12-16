61d386726205cf91ab2f3f5a7c8ca88ac204fe31
Gabriel Philippi <gabriel.philippi@redix.com.br>
2025-12-16 15:06:27 -0300

    a

--- terraform-magalu/environments/staging/variables.tf
+++ terraform-magalu/environments/staging/variables.tf
@@ -2,21 +2,21 @@ variable "api_key" {
   type        = string
   sensitive   = true
   description = "The Magalu Cloud API Key"
-  default     = ""
+  default     = "c56d2787-add8-45ed-a3cf-3a2dca260566"
 }
 
 variable "mgc_key_pair_id" {
   type        = string
   sensitive   = true
   description = "The Magalu Cloud key pair id"
-  default     = ""
+  default     = "944542b2-2e7d-4157-9550-ad10ae2be52b"
 }
 
 variable "mgc_key_pair_secret" { 
     type        = string
     sensitive   = true
     description = "The Magalu Cloud key pair secret"
-    default     = ""
+    default     = "9106239d-2f4a-4076-b6a7-8b763e4ab311"
 }
 
 variable "region" {
